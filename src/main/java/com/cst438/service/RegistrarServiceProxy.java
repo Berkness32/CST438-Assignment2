dde2e25826b29139e5727364d0f498045ab98701
Aaron Berkness <aaronberkness@gmail.com>
2024-07-29 15:51:05 -0700

    Accidentally put the imports in the wrong place. Fixed it

--- src/main/java/com/cst438/service/RegistrarServiceProxy.java
+++ src/main/java/com/cst438/service/RegistrarServiceProxy.java
@@ -1,9 +1,5 @@
 package com.cst438.service;
 
-public class RegistrarServiceProxy {
-
-    package com.cst438.service;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -11,6 +7,7 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+public class RegistrarServiceProxy {
 
     @Service
     public class RegistrarServiceProxy {
